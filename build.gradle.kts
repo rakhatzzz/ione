ef4949a726f354df92b6d800f11b065fb9c3ef1a
hipiqmpz-sys <hipiqmpz@gmail.com>
2026-05-20 22:03:21 +0500

    fix run

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework:spring-websocket")
+	implementation("org.springframework:spring-messaging")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	compileOnly("org.projectlombok:lombok")
